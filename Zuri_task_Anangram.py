6266b586d45495b1657c61d09039982b46da04c7
1stnewuser <99445756+1stnewuser@users.noreply.github.com>
2022-05-27 00:45:31 +0100

    Update Zuri_task_Anangram.py

--- Zuri_task_Anangram.py
+++ Zuri_task_Anangram.py
@@ -1,12 +1,14 @@
+# declear strings 1,and 2 
 str1 = "good"
 str2 = "doog"
-#Convert both strings to lower case
 
 # Check if both string are of the same length
 if(len(str1) == len(str2)):
+    
     #sort both string 
     sorted_str1 = sorted(str1)
     sorted_str2 = sorted(str2)
+    
     # If sorted char arreyers are same
     if(sorted_str1 == sorted_str2):
         print(str1 + " and " + str2 + " are anagram. ")    
